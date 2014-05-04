bb34017f4984d397a66db3e2927ae0e3839f15a3
Craig Bass <craig@devls.co.uk>
2014-05-04 14:34:48 +0100

    s03nd

--- sound.c
+++ sound.c
@@ -1,6 +1,6 @@
 main(t) {
   for( t = 0;;t+=1) {
-	char val = (t^(t>>3|t<<13)^0x80)&(t>>7|t>>8);
+	char val = (t^(t>>3|t<<13)^0x80)|(t>>7|t>>9);
 	val = (val/2) + 128; //clip the sound
 	putchar(val);
   }
