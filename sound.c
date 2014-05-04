8b98e22c53d0b57721eb1945e7e5b98facb73eb2
Craig Bass <craig@devls.co.uk>
2014-05-04 17:51:14 +0100

    s04nd

--- sound.c
+++ sound.c
@@ -1,6 +1,8 @@
 main(t) {
+  int i = 0;
   for( t = 0;;t+=1) {
-	char val = (t^(t>>3|t<<13)^0x80)|(t>>7|t>>9);
+        i = t>>((t>>7)|t>>i);
+	char val = ((t>>3|t<<7|t>>i)^0x80);	
 	val = (val/2) + 128; //clip the sound
 	putchar(val);
   }
